ceab1d7b492aac1b650f0cc92a1fafb10b845c3d
Andreu <andreu.santos1157@gmail.com>
2026-04-28 19:49:48 -0300

    add: paginas next.js

---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Dropshipping Tool</h1>
+      <p>Automacao de dropshipping com Mercado Livre e AliExpress.</p>
+    </main>
+  );
+}
